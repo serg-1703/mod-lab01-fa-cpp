613b19e6fd44991a146101d61ba74d1b1871bfc8
serg-1703 <vlasov0210serg@gmail.com>
2025-03-16 00:03:51 +0300

    add: faStr1

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,7 +2,32 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    unsigned int count = 0;
+    bool inWord = false; 
+    bool hasDigit = false;
+
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (str[i] == ' ') {
+            if (inWord && !hasDigit) {
+                ++count;
+            }
+            inWord = false;
+            hasDigit = false;
+        } else {
+            if (!inWord) {
+                inWord = true;
+            }
+            if (str[i] >= '0' && str[i] <= '9') {
+                hasDigit = true;
+            }
+        }
+    }
+
+    if (inWord && !hasDigit) {
+        ++count;
+    }
+
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
