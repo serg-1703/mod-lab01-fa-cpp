91e8500b7c79c38b1a97367174a67c4d285cbd09
serg-1703 <vlasov0210serg@gmail.com>
2025-03-16 00:19:19 +0300

    add:faStr3

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,5 +1,6 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <cmath>
 
 unsigned int faStr1(const char *str) {
     unsigned int count = 0;
@@ -66,5 +67,32 @@ unsigned int faStr2(const char *str) {
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    unsigned int totalLength = 0;
+    unsigned int wordCount = 0;
+    bool inWord = false;
+
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (str[i] == ' ') {
+            if (inWord) {
+                ++wordCount;
+                inWord = false;
+            }
+        } else {
+            if (!inWord) {
+                inWord = true;
+            }
+            ++totalLength;
+        }
+    }
+
+    if (inWord) {
+        ++wordCount;
+    }
+
+    if (wordCount == 0) {
+        return 0;
+    }
+    double averageLength = static_cast<double>(totalLength) / wordCount;
+
+    return static_cast<unsigned int>(std::round(averageLength));
 }
